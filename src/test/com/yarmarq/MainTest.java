0079a2aba5d8380b1bef967f814c822a6236975c
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2017-12-11 01:41:04 +0100

    Add junit 5, create first test.

---
+++ src/test/com/yarmarq/MainTest.java
@@ -0,0 +1,12 @@
+package com.yarmarq;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
