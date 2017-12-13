2b947c7bf8dac7e57fe17a1647bb03956e6f8d62
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2017-12-13 16:24:02 +0100

    Fix NBPApiClient.

--- src/main/com/yarmarq/module/NBPApiClient.java
+++ src/main/com/yarmarq/module/NBPApiClient.java
@@ -1,18 +1,18 @@
 package com.yarmarq.module;
 
 import com.yarmarq.serializable.Gold;
-import com.yarmarq.serializable.TRate;
+import com.yarmarq.serializable.Rate;
 import com.yarmarq.serializable.Table;
 
 import java.util.Date;
 import java.util.List;
 
 public class NBPApiClient {
-    public TRate getRate(String code) {
+    public Rate getRate(String code) {
         return null;
     }
 
-    public List<TRate> getRates(String code, Date startdate, Date enddate) {
+    public List<Rate> getRates(String code, Date startdate, Date enddate) {
         return null;
     }
 
