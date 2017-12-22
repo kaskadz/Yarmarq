e88228e719fcd4f9fd478d0d7bd90a8dd66f4330
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2017-12-22 02:34:11 +0100

    Add parameter declarations to graph subcommand.

--- src/main/com/yarmarq/subcommand/GraphSubComm.java
+++ src/main/com/yarmarq/subcommand/GraphSubComm.java
@@ -10,6 +10,30 @@ public class GraphSubComm implements Runnable {
             description = "Print usage help and exit.")
     private boolean usageHelpRequested;
 
+    @Parameters(index = "0", arity = "1", paramLabel = "CODE",
+            description = "Currency code.")
+    private String code;
+
+    @Parameters(index = "1", arity = "1", paramLabel = "START_YEAR",
+            description = "Start year.")
+    private Integer startYear;
+    @Parameters(index = "2", arity = "1", paramLabel = "START_MONTH",
+            description = "Start month.")
+    private Integer startMonth;
+    @Parameters(index = "3", arity = "1", paramLabel = "START_WEEK",
+            description = "Start week of the month.")
+    private Integer startWeekNo;
+
+    @Parameters(index = "4", arity = "1", paramLabel = "END_YEAR",
+            description = "End year.")
+    private Integer endYear;
+    @Parameters(index = "5", arity = "1", paramLabel = "END_MONTH",
+            description = "End month.")
+    private Integer endMonth;
+    @Parameters(index = "6", arity = "1", paramLabel = "END_WEEK",
+            description = "End week of the month.")
+    private Integer endWeekNo;
+
     @Override
     public void run() {
         System.out.println("Graph");
